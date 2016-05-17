bf3d5b608c9379d7a86b11c9bcc4b8036379e344
Pascal <Pascal@VAIO>
2016-05-17 14:54:18 +0200

    vgraph

--- src/git1/bn.java
+++ src/git1/bn.java
@@ -1,16 +1,28 @@
 package git1;
 
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Color;
+
+
 public class bn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		
-		System.out.println("toto");
-		System.out.println("titi");
-		System.out.println("tutu");
-		System.out.println("qsggdf");
-		System.out.println ("rrr");
+		JFrame cadre = new javax.swing.JFrame("Premiere fenetre");
+		JPanel panneau = new JPanel();
+		panneau.setPreferredSize(new Dimension(250, 150));
+
+		panneau.setBackground(Color.RED);
+		cadre.setContentPane(panneau);
+		cadre.setLocation(400, 300);
+		cadre.pack();
+		cadre.setVisible(true);
+		cadre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
 		
